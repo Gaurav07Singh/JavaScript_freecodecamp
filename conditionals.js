84d3779683bb5ed1256d205078c5c56cd7bfa7f6
Gaurav Singh <83450530+Gaurav07Singh@users.noreply.github.com>
2022-07-19 20:46:15 +0530

    Add files via upload

---
+++ conditionals.js
@@ -0,0 +1,13 @@
+var a = 10;
+
+if(a > 00) {
+    console.log("Greater than 0")
+} else if(a < 0) {
+    console.log("Smaller than 0");
+} else {
+    console.log("0");
+}
+
+for(i = 0 ;i<=5 ; i++) {
+    console.log(i); 
+}
